dfedb538e3924c9d4b889d2ebdc31b5def2aab35
Jmanugget <33736087+Jmanugget@users.noreply.github.com>
2017-11-21 19:53:11 -0700

    Update audio.js

--- js/app/audio/audio.js
+++ js/app/audio/audio.js
@@ -9,7 +9,7 @@ define(['app/eventmanager', 'app/audio/webaudioprovider', 'app/audio/htmlaudiopr
 	var playingMusic = false;
 	var longloadTimer = false;
 	var playingBossMusic = false;
-	var CDN_PATH = "https://www.github.com/Jmanugget/grid/";
+	var CDN_PATH = "https://github.com/Jmanugget/grid/tree/master/";
 	
 	var sounds = {
 		DayMusic: {
